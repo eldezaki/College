967d48a181f2851910153bf508dd0c621672c84a
eldezaki <152847114+eldezaki@users.noreply.github.com>
2026-09-11 02:24:47 +0000

    Langkah Kerja

---
+++ Kelas 2/PBO/jobsheet3/Main.java
@@ -0,0 +1,9 @@
+package jobsheet3;
+
+public class Main {
+    public static void main(String[] args) {
+        Account acc = new Account("A001", "Nadia", 500000);
+        acc.withdraw(150000);
+        acc.printInfo();
+    }
+}
