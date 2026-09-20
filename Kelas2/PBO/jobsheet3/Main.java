da3dee501c5fc33acee891071e1f7f14173a57cb
eldezaki <152847114+eldezaki@users.noreply.github.com>
2026-09-20 23:00:18 +0000

    aa

--- Kelas2/PBO/jobsheet3/Main.java
+++ Kelas2/PBO/jobsheet3/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Withdraw 300000 allowed? " + limited.withdraw(300000));
         System.out.println("Withdraw 150000 allowed? " + limited.withdraw(150000));
         limited.printInfo();
+        //tes
     }
 }
